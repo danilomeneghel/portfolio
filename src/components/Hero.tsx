ffec3e818713b16f02356b54b578ebb90a103bbf
Danilo Meneghel <danilo.meneghel@gmail.com>
2025-07-04 17:59:39 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,8 @@ const Hero = () => {
           <h4 className="text-4xl md:text-4xl font-bold mb-6">
             <span className="text-gray-100">Desenvolvimento Web</span>
           </h4>
-          
+
+          <br />
           <p className="text-xl md:text-2xl text-gray-300 mb-8 max-w-2xl mx-auto">
             Especialista Java e API REST com mais de 11 anos de experiência em desenvolvimento de sistemas robustos, 
             microserviços e arquiteturas escaláveis.
